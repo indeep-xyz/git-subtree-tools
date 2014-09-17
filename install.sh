1170b25d555ca5cf7547f99ca7f57129304e9989
indeep-xyz <indeep.xyz@gmail.com>
2014-09-17 13:50:16 +0900

    rename init-tools.sh -> setup-tools.sh

--- install.sh
+++ install.sh
@@ -2,11 +2,11 @@
 
 # variables
 MY_DIR="`readlink -f "$0" | sed 's/\/[^\/]*$//'`"
-DEST_NAME="init-gitstt"
+DEST_NAME="setup-gitstt"
 DEST_PATH="/usr/local/bin/$DEST_NAME"
 
 # install
-ln -s "$MY_DIR/init-tools.sh" "$DEST_PATH"
+ln -s "$MY_DIR/setup-tools.sh" "$DEST_PATH"
 
 # message
 [ $? -eq 0 ] && echo "installed to $DEST_PATH"
